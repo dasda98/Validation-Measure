4c2c1b65f3768b8c37b7dfb06df1ce80719f835b
dasda98 <wojniakjakub@gmail.com>
2023-07-18 15:24:11 +0200

    Added KivyMD and changed GUI. Added MDList instead of Text, Clipboard option

--- main.py
+++ main.py
@@ -1,47 +1,63 @@
-from kivy.app import App
-from kivy.core.window import Window
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kivymd.app import MDApp
+from kivymd.uix.list import OneLineListItem, MDList
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.screenmanager import MDScreenManager
 from kivy.lang import Builder
+from kivy.core.clipboard import Clipboard
 
 from measure.metrics import Metric
-
+from config import *
 
 Builder.load_file('gui.kv')
 
-Window.clearcolor = (1, 1, 1, 1.0)
-Window.size = (800, 600)
 
+class User(MDScreen):
 
-class User(Screen):
-    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-    
+
     def submit(self):
-        c_11 = int(self.ids.TP.text)
-        c_00 = int(self.ids.TN.text)
-        c_10 = int(self.ids.FN.text)
-        c_01 = int(self.ids.FP.text)
-        
-        metrics = Metric(c_11=c_11, c_00=c_00, c_10=c_10, c_01=c_01)
+        c_11 = self.ids.TP.text
+        c_00 = self.ids.TN.text
+        c_10 = self.ids.FN.text
+        c_01 = self.ids.FP.text
 
-        results = metrics.all_metrics()
-        to_gui_label = ""
+        if (c_11 != '' and c_11.isnumeric()) and \
+                (c_10 != '' and c_10.isnumeric()) and \
+                (c_01 != '' and c_01.isnumeric()) and \
+                (c_00 != '' and c_00.isnumeric()):
 
-        for k, v in results.items():
-            to_gui_label += f'{k}: {v} \n'
+            metrics = Metric(c_11=int(c_11), c_00=int(c_00), c_10=int(c_10), c_01=int(c_01))
+
+            results = metrics.all_metrics()
+
+            self.ids.container.clear_widgets()
+
+            for k, v in results.items():
+                self.ids.container.add_widget(
+                    OneLineListItem(text=f'[b]{k}:[/b]   {v} \n', on_release=self._copy_text_to_clipboard)
+                )
+
+    def _copy_text_to_clipboard(self, onelinelistitem):
+        text = onelinelistitem.text
+
+        Clipboard.copy(onelinelistitem.text)
+
+    def clear(self):
+        self.ids.TP.text = ''
+        self.ids.TN.text = ''
+        self.ids.FN.text = ''
+        self.ids.FP.text = ''
 
 
-        self.ids.results_measure.text = str(to_gui_label)
-
-
-class RootWidget(ScreenManager):
+class RootWidget(MDScreenManager):
     pass
 
 
-class Main(App):
-    
+class Main(MDApp):
+
     def build(self):
+        self.title = "Measure calculator"
         return RootWidget()
 
 
